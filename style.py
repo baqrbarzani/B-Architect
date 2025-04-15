ddd78977da63b50a8c8340de662278263622dda3
baqrbarzani <baqr.00934879@gmail.com>
2025-04-15 14:31:10 +0300

    Update style.py

--- style.py
+++ style.py
@@ -4,8 +4,24 @@ def apply_custom_style():
     st.markdown("""
         <style>
         .stApp {
-            background-color: #121212;
+            background-image: url("projects/Baqrbarzani.jpg");
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+            position: relative;
+            z-index: 1;
             color: #f0f0f0;
+        }
+
+        .stApp::before {
+            content: "";
+            position: fixed;
+            top: 0;
+            left: 0;
+            height: 100%;
+            width: 100%;
+            background: rgba(0, 0, 0, 0.7);  /* dark overlay */
+            z-index: -1;
         }
 
         html, body, [class*="css"] {
@@ -13,7 +29,6 @@ def apply_custom_style():
             font-size: 16px;
             line-height: 1.7;
             color: #f0f0f0;
-            background-color: #121212;
         }
 
         section[data-testid="stSidebar"] {
@@ -28,18 +43,6 @@ def apply_custom_style():
             letter-spacing: 0.5px;
         }
 
-        h1 {
-            font-size: 2.2rem;
-        }
-
-        h2 {
-            font-size: 1.6rem;
-        }
-
-        h3 {
-            font-size: 1.3rem;
-        }
-
         .markdown-text-container {
             font-size: 1.05rem;
             color: #dddddd;
@@ -48,13 +51,11 @@ def apply_custom_style():
         .stButton>button {
             background-color: #523A28 !important;
             color: white !important;
-            border: none;
             border-radius: 8px;
             padding: 0.6rem 1.3rem;
             font-size: 1rem;
             font-weight: 500;
             transition: 0.3s ease;
-            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.4);
         }
 
         .stButton>button:hover {
@@ -66,16 +67,11 @@ def apply_custom_style():
             color: #f0f0f0 !important;
             border-radius: 8px !important;
             border: 1px solid #444 !important;
-            padding: 0.5rem !important;
         }
 
         img {
             border-radius: 10px;
             box-shadow: 0 2px 10px rgba(0,0,0,0.5);
-        }
-
-        .element-container {
-            margin-bottom: 1.5rem;
         }
         </style>
     """, unsafe_allow_html=True)
