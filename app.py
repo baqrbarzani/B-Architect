0cf91b159dbfaad901cf4b816cb02439b60970a0
baqrbarzani <baqr.00934879@gmail.com>
2025-04-15 14:09:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,18 @@
 import streamlit as st
-import os
-from PIL import Image
-
-# Set page config FIRST
-st.set_page_config(page_title="Architectural Studio", layout="wide")
-
-from style import apply_custom_style
-from contact import show_contact
 from home import show_home
+from contact import show_contact
 from project import show_projects
+from style import apply_custom_style
 
-# Apply styling
+st.set_page_config(page_title="B-Architect Studio", layout="wide")
 apply_custom_style()
 
-# Sidebar navigation
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Projects", "Contact"])
+# Navigation
+pages = {
+    "Home": show_home,
+    "Projects": show_projects,
+    "Contact": show_contact
+}
 
-# Route pages
-if page == "Home":
-    show_home()
-
-elif page == "Projects":
-    show_projects()
-
-elif page == "Contact":
-    show_contact()
+selected_page = st.sidebar.radio("Navigate", list(pages.keys()))
+pages[selected_page]()
